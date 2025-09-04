708912cbaa2d715fe5802c9d23b1746fdc5e5244
Lorenas Bartkus <lorenasbartkus@gmail.com>
2025-09-04 09:18:11 +0300

    Add Create methods in repositories for user and post

--- cmd/api/health.go
+++ cmd/api/health.go
@@ -4,7 +4,7 @@ import "net/http"
 
 func (app *application) healthCheckHandler(w http.ResponseWriter, r *http.Request) {
 	w.WriteHeader(http.StatusOK)
-	w.Write([]byte("OK"))
+	w.Write([]byte("OK"))4
 
 	app.repo.Posts.Create(r.Context())
 }
--- internal/store/users.go
+++ internal/store/users.go
@@ -5,10 +5,38 @@ import (
 	"database/sql"
 )
 
-type UsersStore struct {
+type User struct {
+	ID        int64  `json:"id"`
+	Username  string `json:"username"`
+	Email     string `json:"email"`
+	Password  string `json:"-"`
+	CreatedAt string `json:"created_at"`
+}
+
+type UserStore struct {
 	db *sql.DB
 }
 
-func (store *UsersStore) Create(ctx context.Context) error {
+func (store *UserStore) Create(ctx context.Context, user *User) error {
+	query := `
+		INSERT INTO users (username, email, password) 
+		VALUES ($1, $2, $3) returning id, created_at
+		`
+
+	err := store.db.QueryRowContext(
+		ctx,
+		query,
+		user.Username,
+		user.Email,
+		user.Password,
+	).Scan(
+		&user.ID,
+		&user.CreatedAt,
+	)
+
+	if err != nil {
+		return err
+	}
+
 	return nil
 }
